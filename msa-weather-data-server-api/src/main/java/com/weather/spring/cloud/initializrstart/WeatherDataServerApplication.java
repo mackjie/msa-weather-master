22581d807864ede253f7ef96d9928ac2bfd182d1
17890 <whl1789015971@163.com>
2019-01-09 09:06:04 +0800

    展示页面天气效果布局

--- msa-weather-data-server-api/src/main/java/com/weather/spring/cloud/initializrstart/WeatherDataServerApplication.java
+++ msa-weather-data-server-api/src/main/java/com/weather/spring/cloud/initializrstart/WeatherDataServerApplication.java
@@ -3,11 +3,13 @@ package com.weather.spring.cloud.initializrstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 17890
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WeatherDataServerApplication {
 
 	public static void main(String[] args) {
