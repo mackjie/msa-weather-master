538be887f94248a2172cc8e6a046a3509f5b67b8
17890 <whl1789015971@163.com>
2019-01-11 09:29:27 +0800

    展示页面天气效果布局

--- msa-weather-report-api/src/main/java/com/weather/spring/cloud/initializrstart/WeatherReportApplication.java
+++ msa-weather-report-api/src/main/java/com/weather/spring/cloud/initializrstart/WeatherReportApplication.java
@@ -2,7 +2,6 @@ package com.weather.spring.cloud.initializrstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
